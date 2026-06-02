9c76872a036abcf56de904fabdd40be18eeba054
lstamellos <lstamellos@gmail.com>
2026-06-02 03:41:47 +0300

    Add live exposed text dock controls

--- src/plugin-main.h
+++ src/plugin-main.h
@@ -1,4 +1,9 @@
 #pragma once
 
-#define PLUGIN_NAME    "obs-titles"
+#ifndef PLUGIN_NAME
+#define PLUGIN_NAME "obs-titler-pro"
+#endif
+
+#ifndef PLUGIN_VERSION
 #define PLUGIN_VERSION "1.0.0"
+#endif
